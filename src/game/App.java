84f108da78dacca76b4a0b560586d9589c291d6c
henryh1570 <henryhu@cpp.edu>
2017-05-19 23:59:28 -0700

    win conditions

--- src/game/App.java
+++ src/game/App.java
@@ -6,13 +6,20 @@ public class App {
 
 	public static void main(String[] args) {
 		ArrayList<Piece> list = new ArrayList<Piece>();
-		list.add(new Piece(1, 2, -1));
-		list.add(new Piece(2, 2, -1));
-		list.add(new Piece(3, 2, -1));
-		list.add(new Piece(4, 2, -1));
+		list.add(new Piece(1, 2, 1));
+		list.add(new Piece(2, 2, 2));
+		list.add(new Piece(3, 2, 1));
+		list.add(new Piece(4, 2, 1));
+		
 		Gameboard game = new Gameboard(list);
-
 		System.out.println(game.toString());
-		System.out.println(game.isGameOver());
+		int gameStatus = game.isGameOver(3, 2);
+		if (gameStatus > 0) {
+			System.out.println("Player wins.");
+		} else if (gameStatus < 0) {
+			System.out.println("Opponent wins.");
+		} else {
+			System.out.println("Game is not over.");
+		}
 	}
 }
--- src/game/Gameboard.java
+++ src/game/Gameboard.java
@@ -15,76 +15,57 @@ public class Gameboard {
 	}
 
 	// Given a point, check the same row and column for a 4 line.
-	public boolean isGameDead(int x, int y) {
-		int horizontalCounter = 0;
-		int verticalCounter = 0;
+	// Should be called right after a player makes a move.
+	public int isGameOver(int x, int y) {
+		int playerHorizontalCounter = 0;
+		int playerVerticalCounter = 0;
+		int opponentHorizontalCounter = 0;
+		int opponentVerticalCounter = 0;
+
+		// Consecutive 4 pieces returns true.
 		for (int i = 0; i < BOARD.length; i++) {
 
-			// Consecutive 4 returns true. Non-consecutive will reset counter.
-			if (BOARD[i][y] <= 0) {
-				horizontalCounter = 0;
-			} else {
-				horizontalCounter++;
-			}
-
-			if (horizontalCounter == 4) {
-				return true;
-			}
-
-			if (BOARD[x][i] <= 0) {
-				verticalCounter = 0;
-			} else {
-				verticalCounter++;
-			}
-
-			if (verticalCounter == 4) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	// Checks if the current state has a 4 in a row for the player.
-	public boolean isGameOver() {
-		for (int i = 0; i < BOARD.length; i++) {
-			for (int k = 0; k < BOARD.length; k++) {
-
-				// If within boundaries, check vertical/horizontal victory.
-				if (((i + 4) < BOARD.length) && ((k + 4) < BOARD.length)) {
-					if (horizontalCheck(i, k)) {
-						return true;
-					} else if (verticalCheck(i, k)) {
-						return true;
-					}
+			if (BOARD[i][y] != 0) {
+				if (BOARD[i][y] < 0) {
+					playerHorizontalCounter = 0;
+				} else {
+					playerHorizontalCounter++;
 				}
+				// Check if done.
+				if (playerHorizontalCounter == 4) {
+					return 1;
+				} else if (opponentHorizontalCounter == 4) {
+					return -1;
+				}
+			} else {
+				// Reset counters due to break in middle.
+				playerHorizontalCounter = 0;
+				opponentHorizontalCounter = 0;
+			}
+
+			if (BOARD[x][i] != 0) {
+				if (BOARD[x][i] < 0) {
+					playerVerticalCounter = 0;
+					opponentVerticalCounter++;
+				} else {
+					playerVerticalCounter++;
+					opponentVerticalCounter = 0;
+				}
+				// Check if done.
+				if (playerVerticalCounter == 4) {
+					return 1;
+				} else if (opponentVerticalCounter == 4) {
+					return -1;
+				}
+			} else {
+				// Reset counters due to break in middle.
+				playerVerticalCounter = 0;
+				opponentVerticalCounter = 0;
 			}
 		}
-		return false;
+		return 0; // No one wins.
 	}
-
-	// Checks for 4 vertical adjacent pieces downward.
-	// Only use within 4 spaces of board boundaries.
-	public boolean verticalCheck(int i, int k) {
-		for (int y = k; y < (k + 4); y++) {
-			if (BOARD[i][y] <= 0) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	// Checks for 4 horizontal adjacent pieces to the right.
-	// Only use within 4 spaces of board boundaries.
-	public boolean horizontalCheck(int i, int k) {
-		for (int x = i; x < (i + 4); x++) {
-			if (BOARD[x][k] <= 0) {
-				return false;
-			}
-		}
-		return true;
-	}
-
+	
 	public String toString() {
 		String str = "  1 2 3 4 5 6 7 8";
 		char c = 'A';
